65ab4d0aa7d0bba64a5d7d3fc2c479ca2c805412
ImenOun <imen.oun@esprit.tn>
2024-09-10 12:26:50 +0100

    first

---
+++ src/ZooManagment.java
@@ -0,0 +1,12 @@
+public class ZooManagment {
+    int nbrCages=20;
+    String zooName="My Zoo";
+    public static void main(String[] args)
+    {
+
+        ZooManagment zoo=new ZooManagment();
+        System.out.println(zoo.zooName+" comporte "+zoo.nbrCages+" cages");
+        Scanner scanner = new Scanner(System.in);
+        ZooManagment zm1=new ZooManagment()();
+
+}
