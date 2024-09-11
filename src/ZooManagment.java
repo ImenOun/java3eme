6f2c6db363d2078e85b011f3ec20c417d25315fb
ImenOun <imen.oun@esprit.tn>
2024-09-11 19:14:17 +0100

    2eme commit

--- src/ZooManagment.java
+++ src/ZooManagment.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class ZooManagment {
     int nbrCages=20;
     String zooName="My Zoo";
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
-        ZooManagment zoo=new ZooManagment();
-        System.out.println(zoo.zooName+" comporte "+zoo.nbrCages+" cages");
+        ZooManagment zoo = new ZooManagment();
+        System.out.println(zoo.zooName + " comporte " + zoo.nbrCages + " cages");
         Scanner scanner = new Scanner(System.in);
-        ZooManagment zm1=new ZooManagment()();
+        ZooManagment zm1 = new ZooManagment();
+        System.out.println("donner le nom de zoo: ");
+        zm1.zooName=scanner.nextLine();
+        System.out.println("Donner le nombre de cages de zoo: ");
+        zm1.nbrCages = scanner.nextInt();
+        System.out.println(zm1.zooName+" comporte "+zm1.nbrCages+ " cages");
 
+    }
 }
